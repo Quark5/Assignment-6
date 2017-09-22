cff98437541ede8926717635f1a9a37882c4105f
Quark5 <31390389+Quark5@users.noreply.github.com>
2017-09-22 12:03:43 -0400

    Update app.js

--- server/config/app.js
+++ server/config/app.js
@@ -4,6 +4,7 @@ var config = require('./config'),
 
 module.exports.start = function() {
   var app = express.init();
+  app.set('port', (process.env.PORT || 5000));
   app.listen(app.get('port'), function() {
     console.log('App listening on port', app.get('port'));
   });
