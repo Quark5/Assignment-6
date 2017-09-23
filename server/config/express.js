fd77014c8f1ca41904c82f8e4a436325e6669f8d
Quark5 <31390389+Quark5@users.noreply.github.com>
2017-09-22 20:45:37 -0400

    Update express.js

--- server/config/express.js
+++ server/config/express.js
@@ -9,7 +9,7 @@ var path = require('path'),
 
 module.exports.init = function() {
   //connect to database
-  mongoose.connect(mongodb://Quark5:tianbochen29@ds123371.mlab.com:23371/heroku_l1vtjp76);
+  mongoose.connect(config.db.uri);
 
   //initialize app
   var app = express();
